31c8b0fd7cac3e6e1d4f4194b0a48fa4cdd88fda
Cristian <cpttrabajos@gmail.com>
2023-03-10 23:31:52 -0600

    Simple styles, remove some comments files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_050720) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_11_051033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_050720) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
